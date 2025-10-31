08628081c223ae4ee698dd161cba0935e20805a0
ManhKieuDuy <kieuduymanh26@gmail.com>
2025-10-31 22:01:56 +0700

    Update SendMessage.js

--- Frontend/SendMessage/SendMessage.js
+++ Frontend/SendMessage/SendMessage.js
@@ -1,11 +1,10 @@
-// Lấy danh sách tất cả các công ty (các <li>)
 const chatItems = document.querySelectorAll(".chat-list li");
-// Lấy khung hiển thị tin nhắn bên phải
+
 const chatBox = document.querySelector(".sent-message");
 
 const chatTopName = document.querySelector(".chat-texting-topName");
 
-// Dữ liệu hội thoại mẫu cho từng công ty
+// Hội thoại mẫu cho từng công ty
 const chatScripts = {
     TechVision: [
     { type: 'incoming', text: 'Hello! We have seen your resume and we would like to interview you directly.', avatar: 'techvisionlogo.png' },
@@ -42,17 +41,15 @@ chatItems.forEach(function (item) {
     // Xóa các tin nhắn cũ trong chatBox trước đó
     chatBox.innerHTML = '';
 
-    // Duyệt qua từng tin nhắn trong kịch bản
     messages.forEach(function (msg) {
-      // Tạo một thẻ <div> mới cho mỗi tin nhắn
+      
       var div = document.createElement('div');
-      div.classList.add(msg.type); // Thêm class "incoming" hoặc "outgoing"
+      div.classList.add(msg.type); 
 
-      // Nếu là tin nhắn đến → có avatar
       if (msg.type === 'incoming') {
         div.innerHTML = `<img src="${msg.avatar}"><div class="message">${msg.text}</div>`;
       } 
-      // Nếu là tin nhắn gửi đi → chỉ có nội dung
+    
       else {
         div.innerHTML = `<div class="message">${msg.text}</div>`;
       }
