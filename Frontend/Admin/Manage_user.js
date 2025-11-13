af12455e895a48ef09fda878aec2487de77d5c56
teekawai <mkoolguy2005@gmail.com>
2025-11-14 00:32:07 +0700

    lưu dữ liệu tạo employer vào trong localstorage, thêm nút phân loại role trong delete user

--- Frontend/Admin/Manage_user.js
+++ Frontend/Admin/Manage_user.js
@@ -1,43 +1,3 @@
-// Mock Data
-let jobPosts = [
-	{
-		id: 1,
-		title: "Frontend Developer",
-		company: "TechVision",
-		location: "Hà Nội",
-	},
-	{
-		id: 2,
-		title: "Backend Engineer",
-		company: "SoftWorld",
-		location: "TP.HCM",
-	},
-];
-
-let students = [
-	{ id: 1, name: "Nguyễn Văn A", email: "a@gmail.com" },
-	{ id: 2, name: "Trần Thị B", email: "b@gmail.com" },
-];
-
-let employers = [
-	{ id: 1, name: "Công ty ABC", email: "hr@abc.com" },
-	{ id: 2, name: "Công ty XYZ", email: "contact@xyz.com" },
-];
-
-let users = [
-	{
-		fullName: "Nguyễn Văn A",
-		companyName: "",
-		email: "a@gmail.com",
-		role: "Student",
-	},
-	{
-		fullName: "Công ty ABC",
-		companyName: "ABC",
-		email: "hr@abc.com",
-		role: "Employer",
-	},
-];
 
 // Navigation control
 function showSection(sectionId) {
@@ -47,71 +7,165 @@ function showSection(sectionId) {
 	document.getElementById(sectionId).classList.remove("hidden");
 }
 
-// -------------------- CREATE EMPLOYER --------------------
-document
-	.getElementById("employerForm")
-	.addEventListener("submit", function (e) {
-		e.preventDefault();
-
-		const fullName = document.getElementById("fullName").value.trim();
-		const companyName = document.getElementById("companyName").value.trim();
-		const email = document.getElementById("email").value.trim();
-		const password = document.getElementById("password").value.trim();
-		const phone = document.getElementById("phone").value.trim();
-		const address = document.getElementById("address").value.trim();
-
-		if (!fullName || !companyName || !email || !password) {
-			alert("Please fill all required fields!");
-			return;
+//validate định dạng email, name
+function validate(email, password, employerName, companyName) {
+	const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	const password_pattern = /^.{6,}$/;
+	const error_message_email = document.getElementById('error-message-email');
+	const error_message_password = document.getElementById('error-message-password');
+	const error_message_employer_name = document.getElementById('error-message-employer-name');
+	const error_message_company_name = document.getElementById("error-message-company-name");
+	let validEmail = true;
+	let validPassword = true;
+	let validEmployerName = true;
+	let validCompanyName = true;
+	let isValid = false;
+	//kiểm tra xem điền email chưa
+	if (!email) {
+		error_message_email.innerHTML = "<p>Please enter your email!</p>";
+		validEmail = false;
+	}
+	else {
+		//kiểm tra đúng định dạng không
+		if (!email_pattern.test(email)) {
+			error_message_email.innerHTML = "<p>Incorrect format. Please enter again!</p>";
+			validEmail = false;
+		}
+		else {
+			error_message_email.innerHTML = "";
+			validEmail = true;
 		}
 
-		if (!email.includes("@")) {
-			alert("Invalid email format!");
-			return;
-		}
+	}
 
-		const newEmployer = {
-			fullName,
-			companyName,
-			email,
-			password,
-			phone,
-			address,
-			role: "Employer",
-		};
-		console.log("✅ New Employer Created:", newEmployer);
+	//kiểm tra đã điền mật khẩu chưa
+	if (!password) {
+		error_message_password.innerHTML = "<p>Please enter your password!</p>";
+		validPassword = false;
+	}
+	else {
+		error_message_password.innerHTML = "";
+		validPassword = true;
+	}
 
-		users.push(newEmployer);
-		employers.push({ id: employers.length + 1, name: companyName, email });
-		updateStatistics();
-		alert("Employer created successfully!");
+	//kiểm tra đúng định dạng chưa
+	if (!password_pattern.test(password)) {
+		error_message_password.innerHTML = "<p>Your password must contain at least 6 characters!</p>";
+		validPassword = false;
+	}
+	else {
+		error_message_password.innerHTML = "";
+		validPassword = true;
+	}
 
-		e.target.reset();
-	});
+	//kiểm tra định dạng full name
+	if (!employerName) {
+		error_message_employer_name.innerHTML = "<p>Please enter your name!</p>";
+		validEmployerName = false;
+	}
+	else {
+		error_message_employer_name.innerHTML = "";
+		validEmployerName = true;
+	}
 
-// -------------------- DELETE USER --------------------
+	if (!companyName) {
+		error_message_company_name.innerHTML = "<p>Please enter your company's name!</p>";
+		validCompanyName = false;
+	}
+	else {
+		error_message_company_name.innerHTML = "";
+		validCompanyName = true;
+	}
+
+	if ((validEmail && validPassword && validEmployerName && validCompanyName)) {
+		isValid = true;
+	}
+	return isValid;
+}
+
+// chức năng tạo employer
+document.getElementById("employerForm").addEventListener("submit", function (e) {
+	e.preventDefault();
+
+	const employerName = document.getElementById("employerName").value.trim();
+	const companyName = document.getElementById("companyName").value.trim();
+	const email = document.getElementById("email").value.trim();
+	const password = document.getElementById("password").value.trim();
+	//validate
+	if (!validate(email, password, employerName, companyName)) {
+		return;
+	}
+
+	const newEmployer = {
+		employerName: employerName,
+		companyName: companyName,
+		email: email,
+		password: password,
+		role: "Employer",
+	};
+	console.log("✅ New Employer Created:", newEmployer);
+	//lấy dữ các tài khoản người dùng từ trong local storage
+	let list_user = JSON.parse(localStorage.getItem('list_user')) || {
+		list_student: [],
+		list_employer: []
+	};
+	//đẩy người dùng mới vào mảng
+	list_user.list_employer.push(newEmployer);
+
+	//lưu lại mảng list user mới cập nhật vào local storage
+	localStorage.setItem('list_user', JSON.stringify(list_user))
+	e.target.reset();
+});
+
+//Hiện thông tin các tài khoản theo role
 function renderUserTable(filter = "") {
 	const tbody = document.querySelector("#userTable tbody");
+	const thead = document.querySelector("#userTable thead");
+	let allJob = [];
+	//lấy dữ các tài khoản người dùng từ trong local storage
+	let list_user = JSON.parse(localStorage.getItem('list_user')) || {
+		list_student: [],
+		list_employer: []
+	};
 	tbody.innerHTML = "";
+	//cho tất cả các object(user account) của 2 mảng list student và list employer vào chung 1 mảng 
+	allJob = [...list_user.list_student, ...list_user.list_employer];
+	const roleSelect = document.getElementById('role').value;
+	thead.innerHTML = "";
+	tbody.innerHTML = "";
+	if (roleSelect === "Student") {
+		thead.innerHTML = `
+      <th>Full Name</th>
+      <th>Email</th>
+      <th>Action</th>
+    `;
 
-	users
-		.filter((u) =>
-			Object.values(u).some((val) =>
-				val.toLowerCase().includes(filter.toLowerCase()),
-			),
-		)
-		.forEach((u, i) => {
-			const row = document.createElement("tr");
-			row.innerHTML = `
-        <td>${u.fullName}</td>
-        <td>${u.companyName}</td>
-        <td>${u.email}</td>
-        <td>${u.role}</td>
-        <td><button class="style-button-js"  onclick="deleteUser(${i})">Delete</button></td>
-      `;
-			tbody.appendChild(row);
-		});
+	}
+	if (roleSelect === "Employer") {
+		thead.innerHTML = `
+      <th>Full Name</th>
+      <th>Company Name</th>
+      <th>Email</th>
+      <th>Action</th>
+    `;
+	}
+
+
+
+	// 	.forEach((u, i) => {
+	// 		const row = document.createElement("tr");
+	// 		row.innerHTML = `
+	//     <td>${u.fullName}</td>
+	//     <td>${u.companyName}</td>
+	//     <td>${u.email}</td>
+	//     <td>${u.role}</td>
+	//     <td><button class="style-button-js"  onclick="deleteUser(${i})">Delete</button></td>
+	//   `;
+	// 		tbody.appendChild(row);
+	// 	 });
 }
+//thêm sự kiện thay đổi bảng để tìm student hoặc employer
+document.getElementById("role").addEventListener("change", renderUserTable);
 
 function deleteUser(index) {
 	if (confirm("Are you sure you want to delete this user?")) {
@@ -161,8 +215,8 @@ function showDetails(type) {
     <table border="1" cellpadding="8">
       <thead>
         <tr>${Object.keys(data[0])
-					.map((k) => `<th>${k}</th>`)
-					.join("")}</tr>
+			.map((k) => `<th>${k}</th>`)
+			.join("")}</tr>
       </thead>
       <tbody id="dataTable"></tbody>
     </table>
@@ -187,12 +241,12 @@ function renderTable(list) {
 		.map(
 			(item) => `
     <tr onclick="showItemDetail('${JSON.stringify(item).replace(
-			/"/g,
-			"&quot;",
-		)}')">
+				/"/g,
+				"&quot;",
+			)}')">
       ${Object.values(item)
-				.map((val) => `<td>${val}</td>`)
-				.join("")}
+					.map((val) => `<td>${val}</td>`)
+					.join("")}
     </tr>
   `,
 		)
@@ -203,8 +257,8 @@ function showItemDetail(itemStr) {
 	const item = JSON.parse(itemStr);
 	alert(
 		"DETAIL:\n" +
-			Object.entries(item)
-				.map(([k, v]) => `${k}: ${v}`)
-				.join("\n"),
+		Object.entries(item)
+			.map(([k, v]) => `${k}: ${v}`)
+			.join("\n"),
 	);
 }
